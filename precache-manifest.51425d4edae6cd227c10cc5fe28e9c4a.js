73a074d6f6d0e75e3527f3bd847c68e0ec67606f
Auridel <31318127+Auridel@users.noreply.github.com>
2020-10-14 00:36:59 +0300

    Updates

---
+++ precache-manifest.51425d4edae6cd227c10cc5fe28e9c4a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1497a025957b6ee3ec95c22dcfd1e45b",
+    "url": "/Task_List_App/index.html"
+  },
+  {
+    "revision": "cfb01825e2074e34f6f2",
+    "url": "/Task_List_App/static/css/main.ddc5f4ca.chunk.css"
+  },
+  {
+    "revision": "8ca2f55ac1ff06020ea6",
+    "url": "/Task_List_App/static/js/2.e9500d31.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Task_List_App/static/js/2.e9500d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb01825e2074e34f6f2",
+    "url": "/Task_List_App/static/js/main.702ba745.chunk.js"
+  },
+  {
+    "revision": "3196709cfd3d9413bd9d",
+    "url": "/Task_List_App/static/js/runtime-main.933e5ee5.js"
+  },
+  {
+    "revision": "ea68e3c007db54e4080d66a96e3d5d97",
+    "url": "/Task_List_App/static/media/Ellipse.ea68e3c0.svg"
+  },
+  {
+    "revision": "bf1e8b28aac80632046ce314fb504d60",
+    "url": "/Task_List_App/static/media/black.bf1e8b28.svg"
+  },
+  {
+    "revision": "a756b029507d73d4390f2cfe0aaa7bd2",
+    "url": "/Task_List_App/static/media/blue.a756b029.svg"
+  },
+  {
+    "revision": "5fc9bf11617fafe23446018130309b6c",
+    "url": "/Task_List_App/static/media/check-mark.5fc9bf11.svg"
+  },
+  {
+    "revision": "14e2f379ea2f16993535086cc7b87b36",
+    "url": "/Task_List_App/static/media/close_form.14e2f379.svg"
+  },
+  {
+    "revision": "e0094793367d57e8b278cdb0b6be6512",
+    "url": "/Task_List_App/static/media/completed.e0094793.svg"
+  },
+  {
+    "revision": "d6a93f23c974ae6922fdd0bb52a5e750",
+    "url": "/Task_List_App/static/media/delfolder.d6a93f23.svg"
+  },
+  {
+    "revision": "4a83ea9ba3a5beced03d4f976c782021",
+    "url": "/Task_List_App/static/media/grass.4a83ea9b.svg"
+  },
+  {
+    "revision": "4f919215964cc7217a7ec74271494b2c",
+    "url": "/Task_List_App/static/media/green.4f919215.svg"
+  },
+  {
+    "revision": "d7589ab2529012750f98e1499f306020",
+    "url": "/Task_List_App/static/media/grey.d7589ab2.svg"
+  },
+  {
+    "revision": "340c2c06f8da87c7d02ca47715b234e6",
+    "url": "/Task_List_App/static/media/list-icon.340c2c06.svg"
+  },
+  {
+    "revision": "2814554abfb31c3438acfc084e83aa2a",
+    "url": "/Task_List_App/static/media/pink.2814554a.svg"
+  },
+  {
+    "revision": "6db6bcb9716ce82bb57111ac72c606a4",
+    "url": "/Task_List_App/static/media/plus.6db6bcb9.svg"
+  },
+  {
+    "revision": "ba9a0c8a51696323be441671bb7439c5",
+    "url": "/Task_List_App/static/media/plusSmall.ba9a0c8a.svg"
+  },
+  {
+    "revision": "dfec8c8bb9126b3f07b1c06c9c516a86",
+    "url": "/Task_List_App/static/media/purple.dfec8c8b.svg"
+  },
+  {
+    "revision": "6baab21b676058338fc8bd162882697f",
+    "url": "/Task_List_App/static/media/red.6baab21b.svg"
+  },
+  {
+    "revision": "0747a9acaa5a2ff7530efc53f3c3294f",
+    "url": "/Task_List_App/static/media/task_hovered.0747a9ac.svg"
+  }
+]);
